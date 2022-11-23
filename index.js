1ad0b01a14bc03a8e7583b7ff98336a25b53559f
mbugua <mbuguafaimba@gmail.com>
2022-11-23 15:51:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob"
+function upperCaseCustomerName(){
+    return(window.customerName=customerName.toUpperCase())
+}
+function setBestCustomer(){
+    return (window.bestCustomer='not '+ customerName.toLowerCase())
+}
+function overwriteBestCustomer(){
+    return (window.bestCustomer='maybe '+customerName.toLowerCase())
+}
+const leastFavoriteCustomer = "mary";
+function changeLeastFavoriteCustomer(){
+    
+    return leastFavoriteCustomer ="john"
+}
+
